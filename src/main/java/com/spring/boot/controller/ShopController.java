68a41ec2c9964ada2c7dc86f209ff7684c18658d
Sanjay Bankar <sanjaybankar12@gmail.com>
2017-10-03 22:04:38 +0530

    add shop and updates if already exists

--- src/main/java/com/spring/boot/controller/ShopController.java
+++ src/main/java/com/spring/boot/controller/ShopController.java
@@ -1,9 +1,53 @@
 package com.spring.boot.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.boot.model.Shop;
+import com.spring.boot.service.ShopService;
 
 @RestController
 public class ShopController {
 
+	@Autowired
+	private ShopService shopService;
+	
+	public ShopService getShopService() {
+		return shopService;
+	}
+
+	public void setShopService(ShopService shopService) {
+		this.shopService = shopService;
+	}
+	
+	@GetMapping(value="/getshops")
+	public @ResponseBody String getShops()
+	{
+		return "sanjay";
+	}
+	
+	@PostMapping(value="/shops")
+	public @ResponseBody ResponseEntity addShop(@RequestBody Shop shop)
+	{
+		
+		 Map responseMap=shopService.addShop(shop);
+		 if(responseMap.containsKey("OldShopDetails"))
+		 {
+			 return new ResponseEntity(responseMap,HttpStatus.OK);
+		 }
+		 else
+		 {
+			 return new ResponseEntity(responseMap,HttpStatus.CREATED);
+		 }
+		 
+	}
 	
 }
--- src/main/java/com/spring/boot/dao/impl/ShopDaoImpl.java
+++ src/main/java/com/spring/boot/dao/impl/ShopDaoImpl.java
@@ -43,5 +43,10 @@ public class ShopDaoImpl implements ShopDao{
 			}
 		}
 	}
+	
+	public void getLatLongFromShopAddress(String address,String postCode)
+	{
+		
+	}
 
 }
